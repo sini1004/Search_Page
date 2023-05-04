9e1f23fe1c1221a592736604fdbc29ecfdd2eeda
shinhee <86140799+sini1004@users.noreply.github.com>
2023-05-04 17:55:04 +0900

    Add files via upload

--- search.js
+++ search.js
@@ -20,15 +20,61 @@ let inputWord;
       document.getElementById("frame2").src = `https://dbpedia.org/sparql?default-graph-uri=http%3A%2F%2Fdbpedia.org&query=${query}&format=text%2Fhtml&timeout=30000&signal_void=on&signal_unconnected=on`;
     }
 
-    // function changeIframeUrl3() {
-    //   inputWord = document.getElementById('word').value;
-    //   document.getElementById('frame3').src = `https://www.bing.com/search?q=${inputWord}&form=QBLH&sp=-1&lq=0&pq=${inputWord}&sc=10-3&qs=n&sk=&cvid=BB3A4200C313497F819D7702D6F3F35C&ghsh=0&ghacc=0&ghpl=`;
-    // }
+    function changeIframeUrl3() {
+      inputWord = document.getElementById('word').value;
+      document.getElementById('frame3').src = `https://www.bing.com/search?q=${inputWord}&form=QBLH&sp=-1&lq=0&pq=${inputWord}&sc=10-3&qs=n&sk=&cvid=BB3A4200C313497F819D7702D6F3F35C&ghsh=0&ghacc=0&ghpl=`;
+    console.log(inputWord)
+    }
     
     function changeIframeUrl4() {
+      
+      document.getElementById('tableBody').textContent = "";
       inputWord = document.getElementById('word').value;
-      console.log('4번째 칸');
-      document.getElementById('frame4').innerHTML = `${inputWord}`;
+      var requestURL = `https://viaf.org/viaf/search?query=cql.any%20all%20"${inputWord}"&sortKeys=holdingscount&httpAccept=application/json&recordSchema=info:srw/schema/1/JSON`;
+      var request = new XMLHttpRequest();
+      request.open('GET', requestURL);
+      request.responseType = 'json';
+      request.send();
+      request.onload = function() {
+          var viafData = request.response;
+          var viafDataResponse = [];
+          var viafDataRecords = [];
+          viafDataResponse = viafData.searchRetrieveResponse.records;
+          for(let i = 0; i < viafDataResponse.length; i++) {
+              var temp = [];
+              var temp2;
+              if(viafDataResponse[i].record.recordData.mainHeadings.data.length > 0) {
+                  for(let j = 0; j < viafDataResponse[i].record.recordData.mainHeadings.data.length; j++) {
+                      temp2 = viafDataResponse[i].record.recordData.mainHeadings.data[j].text;
+                      temp.push(temp2);
+                  }
+              } else {
+                  temp2 = viafDataResponse[i].record.recordData.mainHeadings.data.text;
+                  temp.push(temp2);
+              }
+              // console.log(temp2);
+              viafDataRecords.push(temp);
+              // console.log(viafDataRecords[i]);
+          }
+          
+          for(let i = 0; i < viafDataRecords.length; i++) {
+              var myArticle = document.getElementById('tableBody');
+              var tableRow = document.createElement('tr');
+              var myNumber = document.createElement('td');
+              var myData = document.createElement('td');
+              myNumber.textContent = [i + 1];
+              var temp = myArticle.appendChild(tableRow);
+              temp.appendChild(myNumber);
+              
+              for(let j = 0; j < viafDataRecords[i].length; j++) {
+                  var myPara = document.createElement('p');
+                  myPara.textContent = viafDataRecords[i][j];
+                  // console.log(myPara);
+                  myData.appendChild(myPara);
+              }
+              temp.appendChild(myData);
+          }
+      }
     }
 
     function getCheckboxValue() {
@@ -39,13 +85,11 @@ let inputWord;
             changeIframeUrl();
           } else if (item.value === 'dbpedia') {
             changeIframeUrl2();
+          } else if (item.value === 'bing') {
+            changeIframeUrl3();
           } else if (item.value === 'viaf') {
             changeIframeUrl4();
-            console.log('bing없어요');
           } 
-          else {
-            
-          }
         } else {
           
         }
